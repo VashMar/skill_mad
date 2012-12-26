54b31e6727134f814b064bc9214c35b8f370c932
Vash Marada <skillex@ubuntu.(none)>
2012-12-26 10:17:29 -0600

    bucket typo fix

--- config/initializers/S3_CREDENTIALS.rb
+++ config/initializers/S3_CREDENTIALS.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production" 
-   S3_CREDENTIALS = { :access_key_id => ENV['AKIAJH6FJ3EDG5LGKNZAY'], :secret_access_key => ENV['ThkDQegQeEJRX8vdSmk4P7EvyU38d53r73NNVCSX'], :bucket => "skilmadavatars"} 
+   S3_CREDENTIALS = { :access_key_id => ENV['AKIAJH6FJ3EDG5LGKNZAY'], :secret_access_key => ENV['ThkDQegQeEJRX8vdSmk4P7EvyU38d53r73NNVCSX'], :bucket => "skillmadavatars"} 
  else 
    S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
