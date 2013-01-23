f30169b142103aad0c2d986c0a54cfa270815d9e
Vash Marada <skillex@ubuntu.(none)>
2013-01-23 00:19:27 -0600

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121129024142) do
+ActiveRecord::Schema.define(:version => 20130123053632) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20121129024142) do
     t.datetime "updated_at",                     :null => false
     t.integer  "points",      :default => 0,     :null => false
     t.integer  "user_id"
+    t.boolean  "is_private",  :default => false
   end
 
   create_table "votes", :force => true do |t|
