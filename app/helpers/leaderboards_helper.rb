873a0e900b7f6f27e3f0910ea070146b59f675db
Vash Marada <skillex@ubuntu.(none)>
2013-01-26 14:42:23 -0600

    select_tag names

--- app/helpers/leaderboards_helper.rb
+++ app/helpers/leaderboards_helper.rb
@@ -6,8 +6,14 @@ def leaderboard_list(category)
 
 if @category == nil
    Leaderboard.all 
+else
+  @leaderboards = Leaderboard.find_by_category_name(@category.category_name)
+  @leaderboard_list = @leaderboards.collect{|l| [l.leaderboard_name]}
 end
-end 
+    
+end
+
+ 
 
 
 
