31ca7688e9a09b5ae220508342c5ba1b1b175783
Vash Marada <skillex@ubuntu.(none)>
2013-01-26 16:12:30 -0600

    people video swap

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -2,11 +2,26 @@ class LeaderboardsController < ApplicationController
 
 
 def create 
-LeaderBoard.build(params[:leaderboard])
+
 
 end 
 
+def video_board
 
+respond_to do |format|
+ format.js{}
+end 
 
+end
+
+def people_board
+
+@user_list = User.order("points DESC").where(:hasVideo => true)
+
+respond_to do |format|
+ format.js{}
+end 
+
+end
 
 end
