746141e689ed990235393f84cbccbbe3bc8d52ab
Vash Marada <skillex@ubuntu.(none)>
2013-01-26 20:21:01 -0600

    select changes

--- app/helpers/leaderboards_helper.rb
+++ app/helpers/leaderboards_helper.rb
@@ -7,12 +7,21 @@ def leaderboard_list(category)
 if @category == nil
    Leaderboard.all 
 else
-  @leaderboards = Leaderboard.find_by_category_name(@category.category_name)
+  @leaderboards = Leaderboard.find_by_category_name(@category)
+  if @leaderboards 
   @leaderboard_list = @leaderboards.collect{|l| [l.leaderboard_name]}
+  else 
+  @leaderboard_list = ["Boobs"] 
+  end
 end
     
 end
 
+
+def selected_value(category) 
+ @category = category 
+ @category == nil ?  @selected_value = "" :  @selected_value = @category
+end 
  
 
 
