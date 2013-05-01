9dc44ecc5a3dda76880af148d64f923516f85945
pancake <pancake@ubuntu.(none)>
2013-05-01 15:22:48 -0500

    profile picture file size validation; email font styles

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -66,6 +66,12 @@
 
 
   $(document).ready(function(){
+  	$.validator.addMethod('filesize', function(value, element, param) {
+    // param = size (en bytes) 
+    // element = element to validate (<input>)
+    // value = value of the element (file name)
+    return this.optional(element) || (element.files[0].size <= param) 
+});
   	  $.validator.addMethod("isImage", function(value,element){
   		
   		var filetype = $("#form_file").val();
@@ -161,12 +167,13 @@
   	
   	$('#form_pic').validate({
  		rules: {
-  			"user[avatar]": {required: true, isImage:true}
+  			"user[avatar]": {required: true, isImage:true, filesize: 256000 }
 			},
   		messages: {
   			"user[avatar]":{
   				required:"Please select a file",
-  				accepts:"Wrong file type"
+  				accepts:"Wrong file type",
+  				filesize:"File too big, resize to under 250kb"
   			}
   		}
   		});
