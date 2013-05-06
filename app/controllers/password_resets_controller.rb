5b90a6a918f813834057c8605c023e3f51c24c40
vash <vash@skiller.(none)>
2013-05-06 15:14:11 -0500

    forgot password fix

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,9 +29,11 @@ end
 def update
   @user = User.find_by_password_reset_token!(params[:id])
   if @user.password_reset_sent_at < 2.hours.ago
-    redirect_to new_password_reset_path, :failure => "Password reset has expired"
+    flash[:failure] = "That reset token has expired"
+    redirect_to new_password_reset_path
   elsif @user.update_attributes(params[:user])
-    redirect_to home_url, :success => "Password has been reset"
+    flash[:success] = "Password has been reset"
+    redirect_to home_url
   else
     render :edit
   end
